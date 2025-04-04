44ed958e836532cb86be1bb9b81324c18250b0dd
Nao <30605154+NaokoAF@users.noreply.github.com>
2025-04-04 08:14:03 -0300

    Increase in-game sensitivity slider max to 10

---
+++ Core/Patches/GamepadSensitivitySettingPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Unity.Mathematics;
+
+namespace HastyControls.Core.Patches;
+
+[HarmonyPatch(typeof(GamepadSensitivitySetting))]
+internal static class GamepadSensitivitySettingPatch
+{
+	[HarmonyPatch("GetMinMaxValue")]
+	[HarmonyPostfix]
+	static void GetMinMaxValuePostfix(ref float2 __result)
+	{
+		// Increase max controller sensitivity to 10
+		__result.y = 10f;
+	}
+}
