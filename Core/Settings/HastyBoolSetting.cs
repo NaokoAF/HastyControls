ae4c82effcab490df5e203d0dd3234787cbd9674
Nao <30605154+NaokoAF@users.noreply.github.com>
2025-04-05 23:34:47 -0300

    Allow replacing dropdown text on bool and enum settings

--- Core/Settings/HastyBoolSetting.cs
+++ Core/Settings/HastyBoolSetting.cs
@@ -5,17 +5,21 @@ namespace HastyControls.Core.Settings;
 
 public abstract class HastyBoolSetting : BoolSetting, IExposedSetting, IEnumSetting
 {
+	public event Action<bool>? Applied;
+
 	string category;
 	string name;
 	bool defaultValue;
 	LocalizedString displayName;
+	List<string> choices;
 
-	public HastyBoolSetting(string category, string name, bool defaultValue)
+	public HastyBoolSetting(string category, string name, bool defaultValue, string offChoice = "Off", string onChoice = "On")
 	{
 		this.category = category;
 		this.name = name;
 		this.defaultValue = defaultValue;
 		displayName = new(ModInfo.Guid, name);
+		choices = [offChoice, onChoice];
 	}
 
 	public string GetCategory() => category;
@@ -23,11 +27,6 @@ public abstract class HastyBoolSetting : BoolSetting, IExposedSetting, IEnumSett
 	protected override bool GetDefaultValue() => defaultValue;
 	public override LocalizedString OffString => null!;
 	public override LocalizedString OnString => null!;
-
-	List<string> IEnumSetting.GetUnlocalizedChoices()
-	{
-		return new List<string> { "Off", "On" };
-	}
-
-	public override void ApplyValue() { }
+	List<string> IEnumSetting.GetUnlocalizedChoices() => choices;
+	public override void ApplyValue() => Applied?.Invoke(Value);
 }
--- Core/Settings/HastyEnumSetting.cs
+++ Core/Settings/HastyEnumSetting.cs
@@ -3,24 +3,29 @@ using Zorro.Settings;
 
 namespace HastyControls.Core.Settings;
 
-public abstract class HastyEnumSetting<T> : EnumSetting<T>, IExposedSetting where T : unmanaged, Enum
+public abstract class HastyEnumSetting<T> : EnumSetting<T>, IEnumSetting, IExposedSetting where T : unmanaged, Enum
 {
+	public event Action<T>? Applied;
+
 	string category;
 	string name;
 	T defaultValue;
 	LocalizedString displayName;
+	List<string> choices;
 
-	public HastyEnumSetting(string category, string name, T defaultValue)
+	public HastyEnumSetting(string category, string name, T defaultValue, IEnumerable<string>? choices = null)
 	{
 		this.category = category;
 		this.name = name;
 		this.defaultValue = defaultValue;
 		displayName = new(ModInfo.Guid, name);
+		this.choices = choices != null ? new(choices) : new(Enum.GetNames(typeof(T)));
 	}
 
 	public string GetCategory() => category;
 	public LocalizedString GetDisplayName() => displayName;
 	protected override T GetDefaultValue() => defaultValue;
 	public override List<LocalizedString> GetLocalizedChoices() => null!;
-	public override void ApplyValue() { }
+	List<string> IEnumSetting.GetUnlocalizedChoices() => choices;
+	public override void ApplyValue() => Applied?.Invoke(Value);
 }
