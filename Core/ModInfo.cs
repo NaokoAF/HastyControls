18357e005b44fd55bea6dc39457f481b3f2ff2e4
Nao <30605154+NaokoAF@users.noreply.github.com>
2025-04-14 17:15:01 -0300

    Change version to 0.3.0

--- Core/ModInfo.cs
+++ Core/ModInfo.cs
@@ -5,5 +5,5 @@ public static class ModInfo
 	public const string Guid = "NaokoAF.HastyControls";
 	public const string Name = "HastyControls";
 	public const string Author = "NaokoAF";
-	public const string Version = "0.2.2";
+	public const string Version = "0.3.0";
 }
